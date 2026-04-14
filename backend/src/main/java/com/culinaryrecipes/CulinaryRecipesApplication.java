390d674a2ed5e59d20698bb0d6cac43251bb566d
Ayoub Morchid <ayoubmorchid.jobs@gmail.com>
2026-04-14 14:52:09 +0100

    chore: add Spring Boot backend base

---
+++ backend/src/main/java/com/culinaryrecipes/CulinaryRecipesApplication.java
@@ -0,0 +1,12 @@
+package com.culinaryrecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulinaryRecipesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CulinaryRecipesApplication.class, args);
+    }
+}
+
