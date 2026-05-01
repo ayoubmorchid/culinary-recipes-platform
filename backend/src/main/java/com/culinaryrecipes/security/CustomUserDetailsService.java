86eec41f2ab1ad106622585a805d6e3b1dc041b3
Ayoub Morchid <ayoubmorchid.jobs@gmail.com>
2026-05-01 13:55:19 +0100

    security(auth): load users for authentication

---
+++ backend/src/main/java/com/culinaryrecipes/security/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.culinaryrecipes.security;
+
+import com.culinaryrecipes.users.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                Collections.emptyList()
+        );
+    }
+}
