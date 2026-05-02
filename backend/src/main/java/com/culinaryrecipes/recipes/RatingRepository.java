af397b384e9983d43313615d04ebe2c60870f5cb
Ayoub Morchid <ayoubmorchid.jobs@gmail.com>
2026-05-02 15:43:30 +0100

    data(ratings): add recipe rating lookup

--- backend/src/main/java/com/culinaryrecipes/recipes/RatingRepository.java
+++ backend/src/main/java/com/culinaryrecipes/recipes/RatingRepository.java
@@ -2,5 +2,9 @@ package com.culinaryrecipes.recipes;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RatingRepository extends JpaRepository<Rating, Long> {
+
+    List<Rating> findByRecipeSlug(String slug);
 }
