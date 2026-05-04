002149de173895471a9fb4be484215712997e196
Ayoub Morchid <ayoubmorchid.jobs@gmail.com>
2026-05-04 11:34:32 +0100

    layout(app): render navigation bar

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import PublicProfile from "./pages/PublicProfile";
@@ -8,31 +9,42 @@ import CreateRecipe from "./pages/CreateRecipe";
 import Categories from "./pages/Categories";
 import Favorites from "./pages/Favorites";
 import AdminDashboard from "./pages/AdminDashboard";
-import PrivateRoute from "./components/PrivateRoute";
 import Search from "./pages/Search";
+
+import PrivateRoute from "./components/PrivateRoute";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <Routes>
-      <Route path="/users/:username" element={<PublicProfile />} />
-      <Route path="/" element={<div>Home</div>} />
-      <Route path="/categories" element={<Categories />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/favorites" element={<Favorites />} />
-      <Route path="/admin" element={<AdminDashboard />} />
-      <Route path="/search" element={<Search />} />
-      <Route
-        path="/recipes/create"
-        element={
-          <PrivateRoute>
-            <CreateRecipe />
-          </PrivateRoute>
-        }
-      />
-      <Route path="/recipes/:slug" element={<RecipeDetail />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/recipes" element={<Recipes />} />
+    <>
+      <Navbar />
 
-    </Routes>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        <Route path="/users/:username" element={<PublicProfile />} />
+
+        <Route path="/recipes" element={<Recipes />} />
+        <Route path="/recipes/:slug" element={<RecipeDetail />} />
+
+        <Route
+          path="/recipes/create"
+          element={
+            <PrivateRoute>
+              <CreateRecipe />
+            </PrivateRoute>
+          }
+        />
+
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/search" element={<Search />} />
+      </Routes>
+    </>
   );
 }
 
