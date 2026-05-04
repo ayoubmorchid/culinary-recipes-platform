a7becfc3b41c0c88b58e175f3c9a5f14552698ff
Ayoub Morchid <ayoubmorchid.jobs@gmail.com>
2026-05-04 09:26:39 +0100

    frontend(favorites): add favorite toggle client

--- frontend/src/services/favoriteService.js
+++ frontend/src/services/favoriteService.js
@@ -1,9 +1,11 @@
 import axios from "../api/axios";
 
 const favoriteService = {
+  getFavorites: (username) =>
+    axios.get(`/favorites/${username}`),
 
-    getFavorites: (username) =>
-        axios.get(`/favorites/${username}`),
+  toggleFavorite: (username, slug) =>
+    axios.post(`/favorites/${username}/${slug}/toggle`),
 };
 
 export default favoriteService;
