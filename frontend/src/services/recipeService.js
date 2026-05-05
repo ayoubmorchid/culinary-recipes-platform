4f6ae9d30c338585410a81d0db4d7a7262bf0e5c
Ayoub Morchid <ayoubmorchid.jobs@gmail.com>
2026-05-05 23:35:54 +0100

    frontend(recipes): centralize recipe API methods

--- frontend/src/services/recipeService.js
+++ frontend/src/services/recipeService.js
@@ -3,11 +3,14 @@ import axios from "../api/axios";
 const recipeService = {
   getAll: () => axios.get("/recipes"),
 
+  getBySlug: (slug) =>
+    axios.get(`/recipes/${slug}`),
+
   search: (query) =>
     axios.get(`/recipes/search?query=${encodeURIComponent(query)}`),
 
-  getBySlug: (slug) =>
-    axios.get(`/recipes/${slug}`),
+  create: (data) =>
+    axios.post("/recipes", data),
 
   update: (slug, data) =>
     axios.put(`/recipes/${slug}`, data),
