119fff4483df20dd01f0974014fb1aa85272d626
Ayoub Morchid <ayoubmorchid.jobs@gmail.com>
2026-05-06 10:33:33 +0100

    frontend(profile): add profile API client

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -1,16 +1,22 @@
 import axios from "../api/axios";
 
 const userService = {
+  getMyProfile: () => axios.get("/users/me"),
+
+  updateMyProfile: (data) => axios.put("/users/me", data),
+
   uploadAvatar: (file) => {
     const formData = new FormData();
     formData.append("file", file);
 
-    return axios.post("/users/upload-avatar", formData, {
+    return axios.post("/users/me/avatar", formData, {
       headers: {
         "Content-Type": "multipart/form-data",
       },
     });
   },
+
+  getPublicProfile: (username) => axios.get(`/users/${username}`),
 };
 
 export default userService;
